5ebb9076f19b822b26745ebc8311774a2e7ce89d
Azwar Anas <azwarans23@gmail.com>
2026-06-27 16:24:38 +0800

    remove: key in course overview

--- app/features/courses/components/wizard-steps/steps/ReviewSummaryStep.tsx
+++ app/features/courses/components/wizard-steps/steps/ReviewSummaryStep.tsx
@@ -188,10 +188,6 @@ export function ReviewSummaryStep({
                       </p>
                     </div>
                   </div>
-
-                  <div className="flex items-start gap-3">
-                    <Key className="w-5 h-5 text-gray-400 mt-0.5" />
-                  </div>
                 </div>
               </div>
 
